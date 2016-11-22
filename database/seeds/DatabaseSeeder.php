fa6a845b815aa093cfd59a464f2822249c421c63
Dan Trickey <dan@trickey.xyz>
2016-11-22 20:38:08 +0000

    Made Permissions descendant

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
          $this->call(GroupSeeder::class);
          $this->call(PermissionSeeder::class);
+         $this->call(DebugSeeder::class);
     }
 }
