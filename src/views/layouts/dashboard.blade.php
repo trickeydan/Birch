35824687f63d5895932b608794e77fa060543b84
Dan Trickey <dan@trickey.xyz>
2016-12-24 13:29:28 +0000

    Fixed font awesome - temporary
    Temporary fix to #7

--- src/views/layouts/dashboard.blade.php
+++ src/views/layouts/dashboard.blade.php
@@ -8,7 +8,7 @@
         <title>@yield('title') | {{config('birch.site_title')}}</title>
         <!-- Styles -->
         <link href="/css/dashboard.css" rel="stylesheet">
-        <link href="" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
         <!-- Scripts -->
         <script>
